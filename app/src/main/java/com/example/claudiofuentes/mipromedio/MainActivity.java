af99db651ed082861362a9bf6b863d3a531a2884
Claudio Fuentes <claudio-fuentes@hotmail.cl>
2017-12-25 23:48:11 -0300

    Se agrega una nueva vista en blanco, le agregué al boton de acceso la funcionalidad de acceder a esta segunda vista, para volver se usa el boton de volver que tienen los celulares por defecto. Se agrega logo de la sociedad (sin nombre asignado).

--- app/src/main/java/com/example/claudiofuentes/mipromedio/MainActivity.java
+++ app/src/main/java/com/example/claudiofuentes/mipromedio/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.claudiofuentes.mipromedio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botonAcceso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Referencia al botón mediante la carpeta Recursos (R), id (no visible en carpeta).
+        botonAcceso = (Button) findViewById(R.id.btnAcceso);
+
+        //Agregar un listener, es decir la accion.
+        botonAcceso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //es un intento de acceso, es como un link, un hiperVinculo
+                //Parametros de ingreso, donde estoy, hacia donde voy.
+                Intent pantalla =   new Intent(MainActivity.this,Calculador.class);
+                //ejecutar la actividad recien creada
+                startActivity(pantalla);
+                //finalizar la actividad actual para reducir consumo.
+                //finish();
+            }
+        });
     }
 }
